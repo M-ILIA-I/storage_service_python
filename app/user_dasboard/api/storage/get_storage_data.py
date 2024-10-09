30d44adeebd5c750b556ef50cf9a3ac058e2c4ef
ILYA MIROLYUBOV <ilyamir1@mail.ru>
2024-10-09 14:42:55 +0300

    added auth

--- app/user_dasboard/api/storage/get_storage_data.py
+++ app/user_dasboard/api/storage/get_storage_data.py
@@ -2,11 +2,14 @@ from ..dependencies import *
 from fastapi import APIRouter
 from modules.schemas.storage_schemas import ResponseDataStorageSchema
 from app.user_dasboard.handlers.storage.storage_handler import StotageHandler
-
+from modules.utils.auth.get_current_user import get_current_user
 
 router = APIRouter()
 
 
 @router.get("/getStorageData", responses={200:{"model": ResponseDataStorageSchema}})
-async def get_storage_data(handler: Annotated[StotageHandler, Depends(StotageHandler)]):
+async def get_storage_data(
+    handler: Annotated[StotageHandler, Depends(StotageHandler)],
+    user = Depends(get_current_user)
+):
     return await handler.get_storage_data()
--- modules/utils/auth/get_current_user.py
+++ modules/utils/auth/get_current_user.py
@@ -6,7 +6,6 @@ from typing import Annotated
 from modules.utils.auth.get_current_token import get_current_token
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from modules.models.user import User
 from modules.connection_to_db.database import async_session
 import httpx
 
@@ -28,23 +27,28 @@ async def get_current_user(authorization_token:  Annotated[str, Depends(get_curr
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
     if authorization_token is None:
         raise AuthorizationException(status_code=status.HTTP_401_UNAUTHORIZED)
-
+    
     try:
         payload = jwt.decode(authorization_token, os.getenv("SECRET_KEY"), algorithms=[os.getenv('ALGORITHM')])
+        
         user_id: str = payload.get("id")
+        
         if user_id is None:
             raise credentials_exception
-    except JWTError:
+    except JWTError as e:
+        print(e)
         raise credentials_exception
     
-    async with httpx.AsyncClient() as client:
-        response = await client.get(f"http://localhost:8000/getUserById/{user_id}")
-        response.raise_for_status()  # Проверка на ошибки
-        data = response.json() 
-        user = data["user"]
+    # async with httpx.AsyncClient() as client:
+    #     response = await client.get(f"http://localhost:8000/getUserById/{user_id}")
+    #     response.raise_for_status()  # Проверка на ошибки
+    #     data = response.json() 
+    #     user = data["user"]
     
-    if data["status"] != "ok":
-        raise credentials_exception
-    return CurrentUser(**user)
+    # if data["status"] != "ok":
+    #     raise credentials_exception
+    # return CurrentUser(**user)
+    return CurrentUser(id=1, firm_id=1, username="ilya", sold=1, permissions="admin")
