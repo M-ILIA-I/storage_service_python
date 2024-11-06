3beb4917387778d21c6990ac27b55f6f16e5ea81
ILYA MIROLYUBOV <ilyamir1@mail.ru>
2024-11-06 22:48:49 +0300

    update models

--- modules/models/types.py
+++ modules/models/types.py
@@ -4,3 +4,17 @@ from sqlalchemy.orm import relationship, Mapped, declarative_base
 from modules.models.base_model import Base
 import enum
 import uuid
+
+
+class TypeValue(enum.Enum):
+    """
+    int 
+    bool 
+    text
+    double
+    """
+    INT = 1
+    BOOL = 2
+    TEXT = 3
+    DOUBLE = 4
+    TIMESTAMP = 5
--- modules/models/work.py
+++ modules/models/work.py
@@ -1,6 +1,6 @@
 from .types import *
 from .document import Document
-
+    
 
 class Work(Base):
     __tablename__ = "docs_elems_work"
@@ -8,5 +8,10 @@ class Work(Base):
     id: int = Column(Integer, primary_key=True)
     device_id: int = Column(Integer, index=True, nullable=False)
     document_id: int = Column(Integer, ForeignKey("documents.id"))
+    row_number: int = Column(Integer, nullable=False)
+    col_name: str = Column(String, nullable=False)
+    col_value: str = Column(String, nullable=False)
+    type_value: TypeValue = Column("type_value", Enum(TypeValue), nullable=False)
+    dt_change: datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp(), onupdate=func.current_timestamp())
     
     document: Mapped[Document] = relationship(Document)
